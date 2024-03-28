99f74d8a66cc75e9f92f22b909c5784ed428f582
Angshuman Maity <angshuman.maity.iitkgp@gmail.com>
2024-03-28 16:11:16 +0530

    2nd Commit

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-import pandas as pd
+# import pandas as pd
 
-a = input("Please enter the first number - ")
-b = input("Please enter the second number - ")
+# a = input("Please enter the first number - ")
+# b = input("Please enter the second number - ")
+
+def addition (a,b):
+    c = a+b
+    return c
